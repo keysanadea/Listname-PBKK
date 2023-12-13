1ea5e248f66118afe2d331df1e422a1b13a12a51
keysanadea <88714452+keysanadea@users.noreply.github.com>
2023-12-13 19:03:22 +0700

    update

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace listname_latihanwpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+            {
+                lstNames.Items.Add(txtName.Text);
+                txtName.Clear();
+            }
+        }
+
+        private void ButtonDeleteName_Click(object sender, RoutedEventArgs e)
+        {
+            if (lstNames.SelectedItem != null)
+            {
+                lstNames.Items.Remove(lstNames.SelectedItem);
+            }
+        }
+    }
+}
